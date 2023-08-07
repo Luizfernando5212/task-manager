8c659f4117aaf8f5f388d1fac5654eb51dc13924
lfoliveira <luiz.5.2.1.luiz@gmail.com>
2023-08-07 18:39:46 -0300

    Adding the core models of the messaging resource

---
+++ back-end/models/company.js
@@ -0,0 +1,17 @@
+let Schema = mongoose.Schema;
+
+let CompanySchema = new Schema(
+    {
+        name: { type: String, required: true},
+        phone: { type: String, required: true, index: {
+            unique: true,
+            partialFilterExpression: {phone: {$type: "string"}}
+        }},
+        creationDate: { type: Date, required: true, default: Date.now },
+        // address: { type: String, required: true },
+        description: { type: String, required: true },
+        category: { type: String, required: true },
+    }
+);
+
+module.exports = mongoose.model('Company', CompanySchema);
